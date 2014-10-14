95a965a0c930cd9110806fbeb9660c2250913a3e
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2014-10-14 20:56:54 +0200

    added teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004141933) do
+ActiveRecord::Schema.define(version: 20141014185505) do
+
+  create_table "teams", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams_users", id: false, force: true do |t|
+    t.integer "team_id", null: false
+    t.integer "user_id", null: false
+  end
 
   create_table "timelogs", force: true do |t|
     t.datetime "time",        default: '2014-10-13 17:26:38'
